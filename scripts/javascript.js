81bf3b5747e692a9639f4df068ba28261ea15963
YasmineHCMD <145350317+YasmineHCMD@users.noreply.github.com>
2024-10-03 13:35:02 +0200

    Add files via upload

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,1 +1,20 @@
 console.log("Hello world!")
+
+const promo_knop = document.getElementById("socialmedia_knop");
+const promo_menu = document.getElementById("socialmedia_promo");
+
+let stateMenu = false;
+
+function menuToggle(){
+    if(stateMenu == false){
+        promo_menu.style.display = 'flex';
+        stateMenu=true;
+        promo_knop.textContent = 'Verberg ons promotiemateriaal via social media v';
+    } else {
+        promo_menu.style.display = 'none';
+        stateMenu=false;
+        promo_knop.textContent = 'Toon ons promotiemateriaal via social media ^'
+    }
+}
+
+promo_knop.addEventListener('click',menuToggle);
